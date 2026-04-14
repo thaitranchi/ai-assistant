dcca5b38ec6dee2032f7b5feb82fa2a1862bcac8
Trần Chí Thái <31505274+thaitranchi@users.noreply.github.com>
2026-04-14 21:38:21 +0700

    Update model to use 'gemini-1.5-flash'

--- app/services/llm_service.py
+++ app/services/llm_service.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 def ask_llm(prompt: str):
     full_prompt = f"""
